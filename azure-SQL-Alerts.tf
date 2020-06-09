7759bda5bc1eb5e6f6aec3ef5f0051a6acb94fc9
FourTimes <57703276+FourTimes@users.noreply.github.com>
2020-06-09 12:14:37 +0530

    Update azure-SQL-Alerts.tf

--- azure-SQL-Alerts.tf
+++ azure-SQL-Alerts.tf
@@ -1,48 +1,63 @@
+provider "azurerm" {
+  version = "=2.0.0"
+    features {}
+}
+
+data "azurerm_sql_database" "example" {
+  name                = "testDB"
+  server_name         = "jinotestsqlserver"
+  resource_group_name = var.resource_group_name
+}
+
 resource "azurerm_monitor_action_group" "main" {
-  name                = "alert-actiongroup"
-  resource_group_name = "cache-poc"
-  short_name          = "Alerts-triggers"
+  count               = length(var.mailRegister)
+  name                = "alert-actiongroup-${count.index + 1}"
+  resource_group_name = var.resource_group_name
+  short_name          = "Alert-tr-${count.index + 1}"
 
   email_receiver {
     name                    = "Alerts group"
-    email_address           = "52@techmahindra.com"
+    email_address           = element(var.mailRegister, count.index)
     use_common_alert_schema = true
   }
 }
 
-
-data "azurerm_sql_database" "example" {
-  name                = "testDB"
-  server_name         = "testdb-server-jino"
-  resource_group_name = "cache-poc"
-}
-
 locals {
-  metricnames = ["memory_percent", "cpu_percent", "io_consumption_percent", "storage_percent", ""]
+  metricnames = ["cpu_percent", "storage_percent"]
 }
 
 resource "azurerm_monitor_metric_alert" "example" {
-  count               = length(local.storage_percent)
+  count               = length(local.metricnames)
   name                = "SQL-ALERTS-${count.index + 1}"
-  resource_group_name = "cache-poc"
+  resource_group_name = var.resource_group_name
   scopes              = [data.azurerm_sql_database.example.id]
   description         = "warning - ${data.azurerm_sql_database.example.resource_group_name} - ${data.azurerm_sql_database.example.name} - 50% element(local.storage_percent, count.index) used! "
   frequency           = "PT1M"
 
   criteria {
     metric_namespace = "Microsoft.Sql/servers/databases"
-    metric_name      = element(local.storage_percent, count.index)
+    metric_name      = element(local.metricnames, count.index)
     aggregation      = "Maximum"
     operator         = "GreaterThan"
     threshold        = 50
-
   }
-
-  action {
-    action_group_id = azurerm_monitor_action_group.main.id
+dynamic "action" {
+  iterator = pub
+  for_each = azurerm_monitor_action_group.main
+  content {
+    action_group_id =pub.value.id
   }
+}
+}
+
+variable "mailRegister" {
+  default = ["jjino@mail.com","jinojoe@hot.com"]
+}
+
+variable "resource_group_name" {
+  default = "test"
 }
 
 
-
+# https://github.com/hashicorp/terraform-guides/tree/master/infrastructure-as-code/terraform-0.12-examples
 # https://docs.microsoft.com/en-us/azure/azure-monitor/platform/metrics-supported
