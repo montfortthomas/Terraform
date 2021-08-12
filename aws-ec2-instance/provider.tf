dc2f0a823c9e038858e7c194de680d7bce3a408c
fourtimes <57703276+FourTimes@users.noreply.github.com>
2021-08-12 23:30:52 +0530

    Update provider.tf

--- aws-ec2-instance/provider.tf
+++ aws-ec2-instance/provider.tf
@@ -1,10 +1,10 @@
 provider "aws" {
     region = var.region
-    # access_key = var.access_key
-    # secret_key = var.secret_key
+    access_key = var.access_key
+    secret_key = var.secret_key
 }
 
-# terraform {
-#     backend "s3" {}
-# }
+terraform {
+    backend "s3" {}
+}
 
